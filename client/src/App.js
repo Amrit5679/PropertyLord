f2feb827f5a689408fd10bca8b9e49ce943f6905
abhijeet0901 <abhijeetdhardubey90@gmail.com>
2024-03-07 14:50:38 +0530

    Added nav and hero

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,8 @@
-
-import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
-import Navbar from './components/Navbar/Navbar';
-import Register from './components/Auth/Register/Register';
-import Login from './components/Auth/Login/Login';
-
+import "./App.css";
+import Pages from "./components/pages/Pages";
 
 function App() {
-  return (
-    <Router >
-    <Routes>
-      <Route path="/" element={<Navbar />} />
-      <Route path="/login" element={<Login />} /> 
-      <Route path="/register" element={<Register />} /> 
-    </Routes>
-  </Router>
-  );
+  return <Pages />;
 }
 
 export default App;
---
+++ client/src/components/pages/Pages.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../common/header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../home/Home";
+import Footer from "../common/footer/Footer";
+
+const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default Pages;
