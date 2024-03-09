41f18bc8fe3727af3751c870300a46adc8cd705d
Amrit Mishra <mishraamrit929@gmail.com>
2024-03-09 17:35:30 +0530

    Added profile component and modified footer

--- client/src/components/data/Data.js
+++ client/src/components/data/Data.js
@@ -305,14 +305,14 @@ export const price = [
 export const footer = [
   {
     title: "LAYOUTS",
-    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+    text: [{ list: "Home Page",path:"/" }, { list: "About Page",path:"/about" }, { list: "Service Page",path:"/service" }, { list: "Property Page",path:"/property" }, { list: "Contact Page",path:"/contact" }],
   },
   {
     title: "ALL SECTIONS",
-    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }],
   },
   {
     title: "COMPANY",
-    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }],
   },
 ]
--- client/src/components/pages/Pages.jsx
+++ client/src/components/pages/Pages.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Header from "../common/header/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../home/Home";
-
 import AdminLogin from "../Admin/AdminLogin/AdminLogin";
 import AdminReg from "../Admin/AdminReg/AdminReg";
 import Login from "../Auth/Login/Login";
 import Registration from "../Auth/Registration/Registration";
+import ProfileCard from "../Card/ProfileCard";
+import Footer from "../common/footer/Footer";
 
 const Pages = () => {
   return (
@@ -19,9 +20,9 @@ const Pages = () => {
           <Route exact path="/signup" element={<Registration />} />
           <Route exact path="/admin/login" element={<AdminLogin />} />
           <Route exact path="/admin/signup" element={<AdminReg />} />
-
-      
         </Routes>
+        <ProfileCard />
+        <Footer />
       </Router>
     </>
   );
